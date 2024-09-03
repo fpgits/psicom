34c87eb71c47e1c2115cb365dd5cebebfaf71efc
FP <fernandoportelacert@gmail.com>
2024-09-02 22:26:46 -0400

    Update next.config.mjs

--- app-name/next.config.mjs
+++ app-name/next.config.mjs
@@ -1,27 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval';
-  style-src 'self' 'unsafe-inline';
-  img-src 'self' data:;
-`
-
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
-          }
-        ]
-      }
-    ]
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/:path*',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: `
+                default-src 'self';
+                script-src 'self' 'unsafe-eval';
+                style-src 'self' 'unsafe-inline';
+                img-src 'self' data:;
+              `.replace(/\s{2,}/g, ' ').trim()
+            }
+          ]
+        }
+      ]
+    },
+    // Add any other Next.js config options here
   }
-}
+  
+  export default nextConfig
