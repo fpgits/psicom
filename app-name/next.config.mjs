bdae303df817a7632bd9464cee3875d5a1e844c1
FP <fernandoportelacert@gmail.com>
2024-09-02 22:20:33 -0400

    Update next.config.mjs

--- app-name/next.config.mjs
+++ app-name/next.config.mjs
@@ -2,3 +2,26 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data:;
+`
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+          }
+        ]
+      }
+    ]
+  }
+}
