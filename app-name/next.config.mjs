2f8072d614ff7a50f37b0a7ca433e4b4a6b481e9
FP <fernandoportelacert@gmail.com>
2024-09-02 22:53:36 -0400

    f

--- app-name/next.config.mjs
+++ app-name/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     async headers() {
       return [
         {
@@ -9,16 +10,27 @@ const nextConfig = {
               key: 'Content-Security-Policy',
               value: `
                 default-src 'self';
-                script-src 'self' 'unsafe-eval';
+                script-src 'self' 'unsafe-eval' 'unsafe-inline';
                 style-src 'self' 'unsafe-inline';
-                img-src 'self' data:;
+                img-src 'self' data: blob:;
+                font-src 'self';
+                connect-src 'self';
+                frame-src 'self';
+                object-src 'none';
+                base-uri 'self';
+                form-action 'self';
+                frame-ancestors 'none';
+                block-all-mixed-content;
+                upgrade-insecure-requests;
               `.replace(/\s{2,}/g, ' ').trim()
             }
           ]
         }
       ]
     },
-    // Add any other Next.js config options here
+    compiler: {
+      styledComponents: true,
+    },
   }
   
   export default nextConfig
---
+++ app-name/src/app/_document.js
@@ -0,0 +1,17 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body className="__className_0ec1f4">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
