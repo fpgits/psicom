6a491f949cc3cc8c3aa75483752c02294af703fc
FP <fernandoportelacert@gmail.com>
2025-04-06 03:26:01 -0400

    Update next.config.mjs

--- app-name/next.config.mjs
+++ app-name/next.config.mjs
@@ -12,10 +12,10 @@ const nextConfig = {
               default-src 'self';
               script-src 'self' 'unsafe-eval' 'unsafe-inline' https://static.elfsight.com https://universe-static.elfsightcdn.com;
               style-src 'self' 'unsafe-inline';
-              img-src 'self' data: blob: https://static.elfsight.com https://universe-static.elfsightcdn.com;
-              font-src 'self';
+              img-src 'self' data: blob: https://*.gstatic.com https://lh3.googleusercontent.com https://static.elfsight.com https://universe-static.elfsightcdn.com;
+              font-src 'self' https://static.elfsight.com https://fonts.gstatic.com;
               connect-src 'self' https://core.service.elfsight.com https://service-reviews-ultimate.elfsight.com;
-              frame-src 'self' https://apps.elfsight.com https://widgets.elfsight.com;
+              frame-src 'self' https://apps.elfsight.com;
               object-src 'none';
               base-uri 'self';
               form-action 'self';
