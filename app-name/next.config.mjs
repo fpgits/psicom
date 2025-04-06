8c77afd2e24569e787a30c368554369bd2fcb5a2
FP <fernandoportelacert@gmail.com>
2025-04-06 03:20:28 -0400

    Update next.config.mjs

--- app-name/next.config.mjs
+++ app-name/next.config.mjs
@@ -14,8 +14,8 @@ const nextConfig = {
               style-src 'self' 'unsafe-inline';
               img-src 'self' data: blob:;
               font-src 'self';
-              connect-src 'self';
-              frame-src 'self';
+              connect-src 'self' https://core.service.elfsight.com;
+              frame-src 'self' https://apps.elfsight.com;
               object-src 'none';
               base-uri 'self';
               form-action 'self';
